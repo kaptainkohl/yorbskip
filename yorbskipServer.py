a18e64b9d0e92db895de2c8dbb51a3f593ae183a
Blake Kohl <blake.kohl@me.com>
2020-04-29 22:21:25 -0500

    goals updated

--- yorbskipServer.py
+++ yorbskipServer.py
@@ -11,6 +11,7 @@ var_player1 = []
 var_player2 = []
 var_c1 = []
 var_c2 = []
+running =0;
 
 @app.route('/')
 def index():
@@ -31,8 +32,14 @@ def horGoals():
 @socketio.on('my event')
 def test_message(message):
     print(var_seed)
-    emit("load", {'seed': var_seed, 'player1':var_player1,'player2':var_player2,'c1':var_c1,'c2':var_c2});
-    
+    emit("load", {'seed': var_seed, 'player1':var_player1,'player2':var_player2,'c1':var_c1,'c2':var_c2,'start':running});
+ 
+@socketio.on('end')
+def end_message(message):
+    global running
+    running = 0;
+    emit("load", {'seed': var_seed, 'player1':var_player1,'player2':var_player2,'c1':var_c1,'c2':var_c2,'start':running});
+ 
 @socketio.on('clicked')
 def handle_array(data):
     global var_player1
@@ -48,9 +55,19 @@ def handle_array(data):
 @socketio.on('start')
 def handle_start(msg):
     global var_seed
+    global running
+    global var_player1
+    global var_player2
+    global var_c1
+    global var_c2     
     print(msg)
     print(msg['data'])
     var_seed = msg['data']
+    running = msg['start']
+    var_player1 = []
+    var_player2 = []
+    var_c1 = []
+    var_c2 = []
     emit("startgame", msg , broadcast=True)   
 
 
